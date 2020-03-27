46528c0d680335a60125bba1cf572da162d14515
elisego <elise.godard@gmail.com>
2020-03-27 14:08:40 +0100

    Java 08 : Interface

--- Eagle.java
+++ Eagle.java
@@ -1,8 +1,10 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly{
 
+    // attributs
     private boolean flying;
     private int altitude;
 
+    // constructors
     public Eagle(String name) {
         super(name);
         this.flying = false;
@@ -21,4 +23,49 @@ public class Eagle extends Bird {
     public String sing() {
         return "Screech!";
     }
+
+    @Override
+    public void takeOff() {
+        if (!this.flying && this.altitude == 0) {
+            this.flying = true;
+            System.out.println(this.getName() + "takes off in the sky");
+        }
+    }
+
+    @Override
+    public int ascend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.max(this.altitude + meters, 0);
+            System.out.println(this.getName()+" flies upward, altitude : " + this.altitude);
+        }
+        return this.altitude;
+    }
+
+    @Override
+    public int glide() {
+        if (this.flying) {
+            System.out.println("It glides into the air");
+        }
+        return this.altitude;
+        }
+
+    @Override
+    public int descend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.min(this.altitude - meters, 305);
+                System.out.println(this.getName() + " flies downward, altitude :" + this.altitude);
+        }
+        return this.altitude;
+    }
+    @Override
+    public void land() {
+        if (this.flying && this.altitude <= 1) {
+            this.flying = false;
+            System.out.println(this.getName() + "  lands on the ground.");
+        }else{
+            System.out.println(this.getName() +" is too high, it can't lands");
+        }
+    }
 }
+
+
---
+++ Fly.java
@@ -0,0 +1,14 @@
+public interface Fly{
+
+
+    void takeOff(); // ok
+
+    int ascend(int meters); // ok
+
+    int glide(); // ok
+
+    int descend(int meters); // ok
+
+    void land(); // ok
+
+}
